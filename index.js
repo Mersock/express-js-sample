f5ce04d12e4aa1dad4d1499892838330ffa72cec
Phumthawan <knzphumthawan@gmail.com>
2019-06-02 14:30:24 +0700

    add html page

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+//public folder with html page
 app.get('/', (req,res) => {
     console.log(path);
     res.sendFile(path.join(__dirname,'public','index.html'));
