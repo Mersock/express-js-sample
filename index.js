4489a41b8a96dbc5ce2106742be7c16371d7bacd
Phumthawan <knzphumthawan@gmail.com>
2019-06-02 14:09:58 +0700

    index page

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const express = require('express'); 
+//initail
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req,res) => {
+    res.send('hello');
+});
+
+//start at port
+app.listen(PORT, () => {
+    console.log(`Server Started on port ${PORT} `);
+})
