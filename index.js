a3c83186051a50e933f24bc9eb0fe52f42d5ad90
Phumthawan <knzphumthawan@gmail.com>
2019-06-02 20:32:32 +0700

    add template

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 //initail
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const logger = require('./middleware/logger');
 const membersRoute = require('./routes/api/member');
+const members = require('./members');
 
 
 // init middleware
 // app.use(logger);
 
-//request body Parser Middleware
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+//Handlebars midleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+//render index views
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members
+}));
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+//request body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //members api route
 app.use('/api/members', membersRoute);
