8ecfae798e9837ead05d65340d44aa3f62ea4f63
Phumthawan <knzphumthawan@gmail.com>
2019-06-02 16:29:22 +0700

    crud member

--- routes/api/member.js
+++ routes/api/member.js
@@ -28,23 +28,78 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     // res.send(req.body)
     const newMember = {
-        id:uuid(),
-        name:req.body.name,
-        email:req.body.email,
-        username:req.body.username
+        id: uuid(),
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username
     }
 
-    if(!newMember.name || !newMember.email){
+    if (!newMember.name || !newMember.email) {
         return res.status(400).json({
-            'erros':{
-                'status_code':400,
-                'message':`Please include name and email`
+            'erros': {
+                'status_code': 400,
+                'message': `Please include name and email`
             }
         })
     }
 
     members.push(newMember);
-    res.json(members);
+    res.status(201).json(members);
+});
+
+//update member
+router.put('/:id', (req, res) => {
+    const memberId = req.params.id;
+    const found = members.some(member => member.id == memberId);
+    if (found) {
+        const updateMember = req.body;
+        members.map(member => {
+            if (member.id == memberId) {
+                member.name = req.body.name ? req.body.name : member.name;
+                member.email = req.body.email ? req.body.email : member.email;
+                member.username = req.body.username ? req.body.username : member.username;
+
+                res.status(200).json({
+                    'errors': {
+                        'status_code': 200,
+                        'message': `Members is Updated.`,
+                        'member': member
+                    }
+                });
+            }
+        })
+    }
+
+    return res.status(404).json({
+        'errors': {
+            'status_code': 400,
+            'message': `Members id with ${memberId} dose not found.`
+        }
+    });
+
+});
+
+//delete
+router.delete('/:id', (req, res) => {
+    const memberId = req.params.id;
+    const found = members.some(member => member.id == memberId);
+    if (found) {
+        res.status(200).json({
+            'errors': {
+                'status_code': 200,
+                'message': `Members is deleted.`,
+                'members': members.filter(member => member.id != memberId)
+            }
+        });
+    }
+
+    return res.status(404).json({
+        'errors': {
+            'status_code': 400,
+            'message': `Members id with ${memberId} dose not found.`
+        }
+    });
+
 });
 
 module.exports = router;
