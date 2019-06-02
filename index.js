bd0ae032d545319a094c5d6c5773817bc42e1b55
Phumthawan <knzphumthawan@gmail.com>
2019-06-02 14:53:24 +0700

    add middleware' '
    ''
    '

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 //initail
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
+const members = require('./members');
+const logger = require('./middleware/logger');
+
+
+// init middleware
+app.use(logger);
+
+app.get('/api/members', (req, res) => res.json(members));
+
 
 //Set static folder
-app.use(express.static(path.join(__dirname,'public')));
+// app.use(express.static(path.join(__dirname,'public')));
 
 //start at port
 app.listen(PORT, () => {
